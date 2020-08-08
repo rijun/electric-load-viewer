6e93c9ce1df7bd802e0585515035665eb682f491
rijun <richard.jung001@stud.fh-dortmund.de>
2020-08-08 12:58:55 +0200

    Fix responsive issue

--- elv/app.py
+++ elv/app.py
@@ -1,5 +1,7 @@
 import dash
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, meta_tags=[
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+])
 app.config['suppress_callback_exceptions'] = True
 server = app.server
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
 from elv import index
 
-index.app.run_server(debug=True)
+index.app.run_server(debug=True, host='0.0.0.0')
